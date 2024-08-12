4e570115ff9a032e4beccde60021cef58a25eac0
krungkualalumpur <86681413+krungkualalumpur@users.noreply.github.com>
2024-08-12 18:02:09 +0700

    update

--- Packages/Fusion.lua
+++ Packages/Fusion.lua
@@ -1,5 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent._Index["elttob_fusion@0.1.1-beta"]["fusion"])
-export type State = REQUIRED_MODULE.State 
-export type StateOrValue = REQUIRED_MODULE.StateOrValue 
-export type Symbol = REQUIRED_MODULE.Symbol 
-return REQUIRED_MODULE
+return require(script.Parent._Index["elttob_fusion@0.1.1-beta"]["fusion"])
--- Packages/_Index/nightcycle_cold-fusion@8.0.0/Maid.lua
+++ Packages/_Index/nightcycle_cold-fusion@8.0.0/Maid.lua
@@ -1,4 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["nightcycle_maid@1.1.4"]["maid"])
-export type Maid = REQUIRED_MODULE.Maid 
-export type MaidConstructor = REQUIRED_MODULE.MaidConstructor 
-return REQUIRED_MODULE
+return require(script.Parent.Parent["nightcycle_maid@1.1.4"]["maid"])
